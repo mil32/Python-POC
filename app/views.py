533e44e663edb8c40673f0962ff6425e4f0f7f48
Arian Provenzano <aprovenzano@ms.local>
2019-11-08 16:25:17 -0300

    logic of filter in HTML of filter done

--- app/views.py
+++ app/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from app.models import Property
+from app.models import Property, City
 from pprint import pprint
 
 
 def index(request):
     properties = Property.objects.all()
+    cities = City.objects.all()
     context = {
         'properties' : properties,
+        'cities' : cities,
     }
     return render(request, 'app/index.html', context)
 
