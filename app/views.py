cd5e1152e9563106e4ceee955c30035c80a0e528
arianjoan <arianproven@gmail.com>
2019-11-14 19:41:32 -0300

    filters done

--- app/views.py
+++ app/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from app.models import Property, City
-from pprint import pprint
 
 
 def index(request):
     properties = Property.objects.all()
     cities = City.objects.all()
+    city = request.POST.get('cities')
+    price_from = request.POST.get('price_from')
+    price_to = request.POST.get('price_to')
+    max_pax = request.POST.get('max_pax',None)
+    if request.method == 'POST':
+        properties = Property.objects.filter(city=city)
+        if (max_pax != ''):
+            properties = properties.filter(max_pax=max_pax)
+        if (price_from != ''):
+            properties = properties.filter(price__gte=price_from)
+        if (price_to != ''):
+            properties = properties.filter(price__lte=price_to)
     context = {
         'properties' : properties,
         'cities' : cities,
